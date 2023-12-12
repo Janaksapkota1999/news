4c5b09c892853998f38f6ab291843eacf106348f
cheta <cheta@DESKTOP-4F68RE3>
2023-12-12 12:13:26 -0500

    change

---
+++ news/main/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+import requests
+from .models import News, Category,Comment
+def home(request):
+    first_news=News.objects.first()
+    three_news=News.objects.all()[1:3]
+    three_categories=Category.objects.all()[0:3]
+    return render(request,'home.html',{
+        'first_news':first_news,
+        'three_news':three_news,
+        'three_categories':three_categories
+    })
+
+# All News
+def all_news(request):
+    all_news=News.objects.all()
+    return render(request,'all-news.html',{
+        'all_news':all_news
+    })
+
+# Detail Page
+def detail(request,id):
+    news=News.objects.get(pk=id)
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        comment=request.POST['message']
+        Comment.objects.create(
+            news=news,
+            name=name,
+            email=email,
+            comment=comment
+        )
+        messages.success(request,'Comment submitted but in moderation mode.')
+    category=Category.objects.get(id=news.category.id)
+    rel_news=News.objects.filter(category=category).exclude(id=id)
+    comments=Comment.objects.filter(news=news,status=True).order_by('-id')
+    return render(request,'detail.html',{
+        'news':news,
+        'related_news':rel_news,
+        'comments':comments
+    })
+
+# Fetch all category
+def all_category(request):
+    cats=Category.objects.all()
+    return render(request,'category.html',{
+        'cats':cats
+    })
+
+
+# Fetch all category
+def category(request,id):
+    category=Category.objects.get(id=id)
+    news=News.objects.filter(category=category)
+    return render(request,'category-news.html',{
+        'all_news':news,
+        'category':category
+    })
+
+
+def index(request):
+
+
+
+    url = 'https://newsapi.org/v2/everything?q=tesla&from=2023-11-12&sortBy=publishedAt&apiKey=2b5f029d49184c359e7dfbcb7701aee2'
+
+    news_j = requests.get(url).json()
+
+    a = news_j['articles']
+    desc =[]
+    title =[]
+    img =[]
+
+    for i in range(len(a)):
+        f = a[i]
+        title.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+    mylist = zip(title, desc, img)
+
+    context = {'mylist': mylist}
+
+    return render(request, 'index.html', context)
